9a932a5c0209f23c2493e6a31436787931ea42bf
KeleLLXIN <1666490390@qq.com>
2018-06-21 16:45:38 +0800

    cpp

---
+++ helloclass.cpp
@@ -0,0 +1,14 @@
+#include "helloclass.h"
+
+HelloClass::HelloClass()
+{
+cout<<"creat"<<endl;
+}
+HelloClass::~HelloClass()
+{
+cout<<"destroy"<<endl;
+}
+void HelloClass::shello()
+{
+cout<<"hello class"<<endl;
+}
